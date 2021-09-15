8f94626cec21d8c12cff02ea408dbd5e2f17aa25
Hillel B. Price <hp48@duke.edu>
2021-09-14 20:48:18 -0400

    now using 3 inputs

--- test_whatsY.py
+++ test_whatsY.py
@@ -4,10 +4,10 @@ output: 1 y value, float
 """
 import pytest
 
-@pytest.mark.parametrize("pairs,newx,expectedY",[
-    (((1, 2),(2,4)),3,6)
+@pytest.mark.parametrize("pair1,pair2,newx,expectedY",[
+    ((1,2),(2,4),3,6)
 ])
-def test_solveForY(pairs,newx, expectedY):
+def test_solveForY(pair1,pair2,newx, expectedY):
     from whatsY import solveForY
     answer = solveForY(pairs,newx)
     assert answer == expectedY
